f3225f65e8ab3a627a37242f0e1fec83507a31cc
Stefan Pate <stefanpate94@gmail.com>
2023-08-16 18:58:24 -0500

    Solved kekulization issues caused by pyrrole-type things and trouble with explicit hydrogens with postsanitize smiles function from joseph. Built option to enforce templates, in other words pay attention to cofactors used, which is not entirely prescribed by the rule smarts.

--- mapping/map_rxns.py
+++ mapping/map_rxns.py
@@ -4,17 +4,20 @@ import numpy as np
 from mapping_fcns import *
 
 # Run from cmd
-# E.g., python map_rxns.py minimal1224_all_uniprot.tsv ../../bottle/mevalonate_pathway_neutralized.json ../../bottle/mapping_mevalonate_pathway_neutralized_min_rules.csv
+# E.g., python map_rxns.py minimal1224_all_uniprot.tsv 10_true_pos_mc_v21_rxns_rnd_seed_1234.json mapping_test_3.csv
 rules_path = sys.argv[1]
 rxn_dict_path = sys.argv[2]
 save_to = sys.argv[3]
-check_smiles = True # Whether or not to track missing smiles and parse_issues
+check_smiles = True # Whether to save info abt missing smiles & parse_issues
+do_template = True # Whether to enforce template matching, ie cofactors
 path_list = save_to.split('/')
-missing_smiles_path = '/'.join(path_list[:-1]) + '/' + 'missing_smiles_' + path_list[-1].lstrip('mapping_')
-smiles_parse_path = '/'.join(path_list[:-1]) + '/' + 'smiles_parse_issues_' + path_list[-1].lstrip('mapping_')
+missing_smiles_path = 'missing_smiles' + path_list[-1].lstrip('mapping')
+smiles_parse_path = 'smiles_parse_issues' + path_list[-1].lstrip('mapping')
 
 # Set and forget
 stoich_path = 'stoich_metacyc_rxns_directed_221214.json'
+paired_cof_path = 'smi2paired_cof.json'
+unpaired_cof_path = 'smi2unpaired_cof.json'
 
 #################################################################################################
 # Read in rules
@@ -22,39 +25,71 @@ rules = []
 with open(rules_path, 'r') as f:
     reader = csv.reader(f, delimiter='\t')
     for row in reader:
-        rules.append([row[0], row[2]])
+        rules.append(row[:4])
 
 rules = rules[1:] # Remove header
-
 rxn_dict = load_json(rxn_dict_path) # Read in reactions
 stoich_dict = load_json(stoich_path) # Read in stoichiometry
-
 n_rxns = len(list(rxn_dict.keys())) # Total no. reactions to map
 
-# Map reactions to rules
+# Read in cofactor lookup tables
+if do_template:
+    smi2paired_cof = load_json(paired_cof_path)
+    smi2unpaired_cof = load_json(unpaired_cof_path)
+    smi2paired_cof = {tuple(k.split(',')):v[0].split(',') for k,v in smi2paired_cof.items()}
+
 rxn_to_rule = []
 rxns_missing_smiles = []
 rxns_w_smiles_parse_issues = []
 rxn_ctr = 0
 mapped_rxn_binary = np.zeros(shape=(n_rxns,))
-for k, rxn in rxn_dict.items():
+for k, rxn in rxn_dict.items(): # Iterate over rxns
+    # Init data checking flags
+    missing_smiles = False
+    smiles_parse_issue = False
     row = [k]
-    rxn = apply_stoich(k, rxn, stoich_dict)
-    for elt in rules:
-        rule_name, rule_smarts = elt
-        found_match, missing_smiles, smiles_parse_issue = map_rxn2rule(rxn, rule_smarts)
+    rxn = apply_stoich(k, rxn, stoich_dict) # Returns list of lists of smiles
 
-        if found_match:
-            print(f"{k} => {rule_name}")
-            row.append(rule_name)
-            mapped_rxn_binary[rxn_ctr] = 1
+    # Check that the data is okay
+    # Check for missing smiles
+    if (None in rxn[0]) or (None in rxn[1]):
+        missing_smiles = True
+    else:
+        # Try to sanitize and remove stereochem
+        reactants, reactants_parse_issue = sanitize(rxn[0])
+        products, products_parse_issue = sanitize(rxn[1])
+        rxn = [reactants, products] # !!
+    
+        # Catch smiles parse issues
+        if reactants_parse_issue or products_parse_issue:
+            smiles_parse_issue = True
+    
+    # If the data is okay, try to map
+    if (not missing_smiles) & (not smiles_parse_issue):
+        for elt in rules: # Iterate over rules
+            did_map = False # Init mapping flag
+            rule_name, rule_reactants_template, rule_smarts, rule_products_template = elt
 
-        if missing_smiles & ([k] not in rxns_missing_smiles):
-            rxns_missing_smiles.append([k])
+            if do_template:
+                matched_idxs = match_template(rxn, rule_reactants_template, rule_products_template, smi2paired_cof, smi2unpaired_cof)
 
-        if smiles_parse_issue & ([k] not in rxns_w_smiles_parse_issues):
-            rxns_w_smiles_parse_issues.append([k])
-        
+                if len(matched_idxs) > 0:
+                    did_map = map_rxn2rule(rxn, rule_smarts, matched_idxs=matched_idxs) # Map if have template matches
+
+            else:
+                did_map = map_rxn2rule(rxn, rule_smarts) # Map w/o enforcing templates
+
+            if did_map:
+                print(f"{k} => {rule_name}")
+                row.append(rule_name)
+                mapped_rxn_binary[rxn_ctr] = 1
+
+            if missing_smiles & ([k] not in rxns_missing_smiles):
+                rxns_missing_smiles.append([k])
+
+            if smiles_parse_issue & ([k] not in rxns_w_smiles_parse_issues):
+                rxns_w_smiles_parse_issues.append([k])
+            
     rxn_to_rule.append(row)
     rxn_ctr += 1 # Update progress
 
@@ -83,4 +118,3 @@ for k, rxn in rxn_dict.items():
 
             rxns_missing_smiles = []
             rxns_w_smiles_parse_issues = []
-
--- mapping/mapping_fcns.py
+++ mapping/mapping_fcns.py
@@ -1,10 +1,10 @@
 import json
 import numpy as np
-from itertools import permutations
+from itertools import permutations, product
 from rdkit import Chem
 from rdkit.Chem import AllChem, CanonSmiles
 
-def map_rxn2rule(rxn, rule, max_products=10000):
+def map_rxn2rule(rxn, rule, matched_idxs=None, max_products=10000):
     '''
     Maps reactions to SMARTS-encoded reaction rule.
     Args:
@@ -16,41 +16,29 @@ def map_rxn2rule(rxn, rule, max_products=10000):
         - missing_smiles (bool)
         - smiles_parse_issue (bool)
     '''
-    pre_sani_reactants, pre_sani_products = rxn # Get lists of smiles
-
-    # Initialize flags
-    did_map = False
-    missing_smiles = False
-    smiles_parse_issue = False
-
-    # Check for missing smiles
-    if (None in pre_sani_reactants) or (None in pre_sani_products):
-        missing_smiles = True
-        return did_map, missing_smiles, smiles_parse_issue
-    
-    # Try to sanitize and remove stereochem
-    reactants, reactants_parse_issue = sanitize(pre_sani_reactants)
-    products, products_parse_issue = sanitize(pre_sani_products)
-    
-    # Catch smiles parse issues
-    if reactants_parse_issue or products_parse_issue:
-        smiles_parse_issue = True
-        return did_map, missing_smiles, smiles_parse_issue
+    reactants, products = rxn
     
     # If neither missing nor unparseable smiles, continue with mapping
     products = sorted(products)
     operator = Chem.rdChemReactions.ReactionFromSmarts(rule) # Make reaction object from smarts string
-    reactants_mol = tuple([Chem.MolFromSmiles(elt) for elt in reactants]) # Convert reactant smiles to mol obj
+    reactants_mol = [Chem.MolFromSmiles(elt) for elt in reactants] # Convert reactant smiles to mol obj
     rule_substrate_cts = count_reactants(rule) # [n_reactants, n_products] in a rule
     rxn_substrate_cts = [len(reactants), len(products)]
 
     # Check if number of reactants / products strictly match
     # rule to reaction
     if rule_substrate_cts != rxn_substrate_cts:
-        return did_map, missing_smiles, smiles_parse_issue
+        return False
+    
+    # If not enforcing templates,
+    # get all permutations of reactant
+    # indices
+    if matched_idxs is None:
+        matched_idxs = list(permutations([i for i in range(len(reactants))]))
         
     # For every permutation of that subset of reactants
-    for perm in permutations(reactants_mol):
+    for idx_perm in matched_idxs:
+        perm = tuple([reactants_mol[idx] for idx in idx_perm]) # Re-order reactants based on allowable idx perms
         outputs = operator.RunReactants(perm, maxProducts=max_products) # Apply rule to that permutation of reactants
 
         for output in outputs:
@@ -63,10 +51,67 @@ def map_rxn2rule(rxn, rule, max_products=10000):
 
             # Compare predicted to actual products. If mapped, return
             if output == products: 
-                did_map = True
-                return did_map, missing_smiles, smiles_parse_issue
+                return True
+            
+            # Last, try fixing kekulization issues
+            postsan_output = postsanitize_smiles(output)
+            for elt in postsan_output: # Iterate over sets of outputs w/ diff tautomers
+                if sorted(elt) == products:
+                    return True
+    return False
 
-    return did_map, missing_smiles, smiles_parse_issue
+def match_template(rxn, rule_reactants_template, rule_products_template, smi2paired_cof, smi2unpaired_cof):
+    '''
+    Returns the permuted indices corresponding to
+    a match between reactant and rule templates
+    '''
+
+    reactants_smi, products_smi = rxn
+    reactants_template = ['Any' for elt in reactants_smi]
+    products_template = ['Any' for elt in products_smi]
+    rule_reactants_template = tuple(rule_reactants_template.split(';'))
+    rule_products_template = tuple(rule_products_template.split(';'))
+
+    # Search for unpaired cofactors first
+    for i, r in enumerate(reactants_smi):
+        if r in smi2unpaired_cof:
+            reactants_template[i] = smi2unpaired_cof[r]
+
+    for i, p in enumerate(products_smi):
+        if p in smi2unpaired_cof:
+            products_template[i] = smi2unpaired_cof[p]
+
+    # Search for paired cofactors
+    # Only overwriting should be PPi/Pi as phosphate donor/acceptor
+    for i, r in enumerate(reactants_smi):
+        for j, p in enumerate(products_smi):
+            if (r, p) in smi2paired_cof:
+                reactants_template[i] = smi2paired_cof[(r, p)][0]
+                products_template[j] = smi2paired_cof[(r, p)][1]
+            elif (p, r) in smi2paired_cof:
+                reactants_template[i] = smi2paired_cof[(p, r)][1]
+                products_template[j] = smi2paired_cof[(p, r)][0]
+
+    reactants_idx_template = [(elt, i) for i, elt in enumerate(reactants_template)]
+
+    # First try to products templates
+    product_template_match = False
+    for perm in permutations(products_template):
+        if perm == rule_products_template:
+            product_template_match = True
+
+    # If product templates match
+    # find permutations of reactant template that match
+    # rule template and keep the indices of those good permutations
+    # Else return empty list
+    matched_idxs = []
+    if product_template_match:
+        for perm in permutations(reactants_idx_template):
+            this_template, this_idx = list(zip(*perm))
+            if this_template == rule_reactants_template:
+                matched_idxs.append(this_idx)
+
+    return matched_idxs
 
 def apply_stoich(rxn_id, rxn, stoich_dict):
     '''
@@ -143,3 +188,75 @@ def load_json(path):
     with open(path, 'r') as f:
         data = json.load(f)
     return data
+
+def postsanitize_smiles(smiles_list):
+    """Postsanitize smiles after running SMARTS.
+    :returns tautomer list of list of smiles"""
+
+    sanitized_list = []
+    tautomer_smarts = '[#7H1X3&a:1]:[#6&a:2]:[#7H0X2&a:3]>>[#7H0X2:1]:[#6:2]:[#7H1X3:3]'
+
+    for s in smiles_list:
+
+        temp_mol = Chem.MolFromSmiles(s, sanitize=False)
+        aromatic_bonds = [i.GetIdx() for i in temp_mol.GetBonds() if i.GetBondType() == Chem.rdchem.BondType.AROMATIC]
+
+        for i in temp_mol.GetBonds():
+            if i.GetBondType() == Chem.rdchem.BondType.UNSPECIFIED:
+                i.SetBondType(Chem.rdchem.BondType.SINGLE)
+
+        try:
+            Chem.SanitizeMol(temp_mol)
+            Chem.rdmolops.RemoveStereochemistry(temp_mol)
+            temp_smiles = Chem.MolToSmiles(temp_mol)
+
+        except Exception as msg:
+            if 'Can\'t kekulize mol' in str(msg):
+                pyrrole_indices = [i[0] for i in temp_mol.GetSubstructMatches(Chem.MolFromSmarts('n'))]
+
+                # indices to sanitize
+                for s_i in pyrrole_indices:
+                    temp_mol = Chem.MolFromSmiles(s, sanitize=False)
+                    if temp_mol.GetAtomWithIdx(s_i).GetNumExplicitHs() == 0:
+                        temp_mol.GetAtomWithIdx(s_i).SetNumExplicitHs(1)
+                    elif temp_mol.GetAtomWithIdx(s_i).GetNumExplicitHs() == 1:
+                        temp_mol.GetAtomWithIdx(s_i).SetNumExplicitHs(0)
+                    try:
+                        Chem.SanitizeMol(temp_mol)
+
+                        processed_pyrrole_indices = [i[0] for i in
+                                                     temp_mol.GetSubstructMatches(Chem.MolFromSmarts('n'))]
+                        processed_aromatic_bonds = [i.GetIdx() for i in
+                                                    temp_mol.GetBonds() if i.GetBondType() == Chem.rdchem.BondType.AROMATIC]
+                        if processed_pyrrole_indices != pyrrole_indices or aromatic_bonds != processed_aromatic_bonds:
+                            continue
+
+                        Chem.rdmolops.RemoveStereochemistry(temp_mol)
+                        temp_smiles = Chem.MolToSmiles(temp_mol)
+                        break
+                    except:
+                        continue
+                if 'temp_smiles' not in vars():
+                    Chem.rdmolops.RemoveStereochemistry(temp_mol)
+                    temp_smiles = Chem.MolToSmiles(temp_mol)
+                    sanitized_list.append([temp_smiles])
+                    continue
+            else:
+                Chem.rdmolops.RemoveStereochemistry(temp_mol)
+                temp_smiles = Chem.MolToSmiles(temp_mol)
+                sanitized_list.append([temp_smiles])
+                continue
+        rxn = AllChem.ReactionFromSmarts(tautomer_smarts)
+
+        try:
+            tautomer_mols = rxn.RunReactants((Chem.MolFromSmiles(temp_smiles), ))
+        except:
+            try:
+                tautomer_mols = rxn.RunReactants((Chem.MolFromSmiles(temp_smiles, sanitize=False),))
+            except:
+                continue
+
+        tautomer_smiles = [Chem.MolToSmiles(m[0]) for m in tautomer_mols]
+        sanitized_list.append(sorted(set(tautomer_smiles + [temp_smiles])))
+
+    return list(product(*sanitized_list))
